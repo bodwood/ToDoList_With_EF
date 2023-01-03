fd848390c7f25e25c4caaf7a39b9b0480db7b842
bodwood <bodiewood@hotmail.com>
2023-01-02 18:37:42 -0800

    add new itemtags.cs class for many-to-many relationship between items and tags

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Models
     public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
     public DbSet<Tag> Tags { get; set; }  //added for Tag class
+    public DbSet<ItemTag> ItemTags { get; set; }  //added for ItemTag class (ef core creates table for this in database)
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
   }
